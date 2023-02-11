6d11dc9d931bf2cf792078f93813a1d291f63e6c
sakCode <angelitotupapitomana@gmail.com>
2023-02-11 18:07:34 -0500

    video_002

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('template');
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/401', function () {
+    return view('pages.401');
+});
+Route::get('/404', function () {
+    return view('pages.404');
+});
+Route::get('/500', function () {
+    return view('pages.500');
 });
