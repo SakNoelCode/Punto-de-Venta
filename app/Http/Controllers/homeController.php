1289efe51a9e8921d349f50479b54690e521ed72
SakCode <angelitotupapitomana@gmail.com>
2025-05-25 16:18:19 -0500

    video_029

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class homeController extends Controller
 {
@@ -12,6 +14,23 @@ class homeController extends Controller
         if (!Auth::check()) {
             return view('welcome');
         }
-        return view('panel.index');
+
+        $totalVentasPorDia = DB::table('ventas')
+            ->selectRaw('DATE(created_at) as fecha, SUM(total) as total')
+            ->where('created_at', '>=', Carbon::now()->subDays(7))
+            ->groupBy(DB::raw('DATE(created_at)'))
+            ->orderBy('fecha', 'asc')
+            ->get()->toArray();
+
+        $productosStockBajo = DB::table('productos')
+            ->join('inventario', 'productos.id', '=', 'inventario.producto_id')
+            ->where('inventario.cantidad', '>', 0)
+            ->orderBy('inventario.cantidad', 'asc')
+            ->select('productos.nombre', 'inventario.cantidad')
+            ->limit(5)
+            ->get();
+
+
+        return view('panel.index', compact('totalVentasPorDia','productosStockBajo'));
     }
 }
